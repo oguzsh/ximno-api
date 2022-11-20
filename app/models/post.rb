93dfd03a602d9e6176038731e1789de99ef3e2fa
Oguzhan Ince <oguzhan824@gmail.com>
2022-11-20 17:05:32 +0300

    add: image file for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,17 @@
 #
 # Table name: posts
 #
-#  id         :bigint           not null, primary key
-#  user_id    :bigint           not null
-#  body       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :bigint           not null, primary key
+#  user_id        :bigint           not null
+#  body           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  comments_count :integer
 #
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   has_many :comments, dependent: :destroy
+
+  scope :by_recently_created, -> { order(created_at: :desc) }
 end
